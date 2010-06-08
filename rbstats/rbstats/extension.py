155109a7ae07cb61b757a315b2156c89170009a2
Mike <mike@mike-laptop.(none)>
2010-06-08 13:42:51 -0400

    Fiddling with models and resources

--- rbstats/rbstats/extension.py
+++ rbstats/rbstats/extension.py
@@ -2,8 +2,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include
 from reviewboard.extensions.base import Extension
-from reviewboard.extensions.hooks import DashboardHook, URLHook
-
+from reviewboard.extensions.hooks import DashboardHook, URLHook, TemplateHook
+from rbstats.resources import reviewing_session_resource
 
 class RBStatsDashboardHook(DashboardHook):
     def get_entries(self):
@@ -14,9 +14,12 @@ class RBStatsDashboardHook(DashboardHook):
 
 
 class RBStatsExtension(Extension):
+    
     is_configurable = True
     requires = ['rbdefects.extension.RBDefectsExtension']
-
+    resources = [reviewing_session_resource]
+    
     def __init__(self):
         Extension.__init__(self)
         self.dashboard_hook = RBStatsDashboardHook(self)
+        #TemplateHook(self, "base-scripts-post", "bears/bears.html", ['dashboard'])
